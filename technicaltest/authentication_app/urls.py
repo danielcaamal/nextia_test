22e39b868e618ec57ee6206d06475d221c59f975
daniel.caamal <danielcaamal97@gmail.com>
2022-04-13 21:29:21 -0500

    feature: urls added, auth views implemented (register, login logout), User CRUD implemented

--- technicaltest/authentication_app/urls.py
+++ technicaltest/authentication_app/urls.py
@@ -6,4 +6,9 @@ from authentication_app import views
 
 # URL patterns for the users_app 
 urlpatterns = [
+    # user routes
+    path('user', views.UserView.as_view(), name='user'),
+    path('user/register', views.RegisterView.as_view(), name='user-register'),
+    path('user/login', views.LoginView.as_view(), name='user-login'),
+    path('user/logout', views.LogoutView.as_view(), name='user-logout'),
 ]
